b893fae69a7d2685ad838ff55c330734adeeaa80
OmriRoter <131654937+OmriRoter@users.noreply.github.com>
2024-07-11 06:29:53 +0300

    Refactored MainActivity and other classes for better modularity and maintainability.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,10 +19,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     compileOptions {
@@ -32,12 +29,16 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.play.services.maps)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.crashlytics.buildtools)
+    implementation(libs.play.services.location)
+    implementation(libs.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "SpaceObstacleRace"
 include(":app")
- 
